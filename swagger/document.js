7f3636232a18c6f47a402561e292819237afd5d2
shuvam-mohapatra <65060273+shuvam-mohapatra@users.noreply.github.com>
2022-12-02 11:11:05 +0530

    updatedPathSwagger

--- swagger/document.js
+++ swagger/document.js
@@ -1,38 +1,38 @@
-const getGuestDetails = require("../swagger/bookingMaster/guestDetailsSwagger");
+const getGuestDetails = require("./admin/bookingMaster/guestDetailsSwagger");
 
 const swaggerDocumentation = {
-	openapi: "3.0.0",
-	info: {
-		title: "Gods Own Country",
-		version: "1.0.0",
-		description: "API Documentations",
-	},
-	servers: [
-		{
-			url: "http://localhost:2800/",
-			description: "Local dev",
-		},
-	],
-	basePath: "/",
-	tags: [
-		{
-			name: "Guest",
-		},
-	],
-	paths: {
-		"/guest/list": {
-			get: getGuestDetails.getGuestDetailsSwagger,
-		},
-		"/guest/create": {
-			post: getGuestDetails.addGuestDetailsSwagger,
-		},
-		"/guest/update/{id}": {
-			put: getGuestDetails.updateGuestDetailsSwagger,
-		},
-		"/guest/delete/{id}": {
-			delete: getGuestDetails.deleteGuestDetailsSwagger,
-		},
-	},
+  openapi: "3.0.0",
+  info: {
+    title: "Gods Own Country",
+    version: "1.0.0",
+    description: "API Documentations",
+  },
+  servers: [
+    {
+      url: "http://localhost:2800/",
+      description: "Local dev",
+    },
+  ],
+  basePath: "/",
+  tags: [
+    {
+      name: "Guest",
+    },
+  ],
+  paths: {
+    "/guest/list": {
+      get: getGuestDetails.getGuestDetailsSwagger,
+    },
+    "/guest/create": {
+      post: getGuestDetails.addGuestDetailsSwagger,
+    },
+    "/guest/update/{id}": {
+      put: getGuestDetails.updateGuestDetailsSwagger,
+    },
+    "/guest/delete/{id}": {
+      delete: getGuestDetails.deleteGuestDetailsSwagger,
+    },
+  },
 };
 
 module.exports = swaggerDocumentation;
